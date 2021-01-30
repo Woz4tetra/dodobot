309b4eeeac2024b277346a9526e5646fde52fd9b
Woz4tetra <woz4tetra@gmail.com>
2021-01-29 22:15:25 -0500

    adding configurations for 2D and 3D plotter

--- particle_filter/main.py
+++ particle_filter/main.py
@@ -1,7 +1,7 @@
 import math
 import time
 import numpy as np
-from plotter import ParticleFilterPlotter
+from plotter import ParticleFilterPlotter3D, ParticleFilterPlotter2D
 
 from state_loader import read_pkl
 from particle_filter import ParticleFilter
@@ -50,9 +50,9 @@ class InputVector:
 
 def main():
     # path = "data/objects_2021-01-06-23-36-19.json"
-    # path = "data/objects_2021-01-06-23-37-06.json"
+    path = "data/objects_2021-01-06-23-37-06.json"
     # path = "data/objects_2021-01-24-18-58-26.json"
-    path = "data/objects_2021-01-24-18-59-28.json"
+    # path = "data/objects_2021-01-24-18-59-28.json"
 
     # repickle = True
     repickle = False
@@ -60,33 +60,38 @@ def main():
     run_pf(states)
 
 
-def run_pf(states):
-    initial_state = None
-    initial_range = [1.0, 1.0, 1.0]
-    for state in states:
-        # if state.type == "blue_cut_sphere":
-        if state.type == "red_cut_sphere":
-            if initial_state is None:
-                initial_state = [state.x, state.y, state.z]
-                break
-    assert initial_state is not None
-    meas_std_val = 0.01
-    pf = ParticleFilter(250, meas_std_val)
-    plotter = ParticleFilterPlotter(pf, 3.0, 3.0, 3.0)
-
+def init_pf(num_particles, meas_std_val, initial_range, initial_state):
+    pf = ParticleFilter(num_particles, meas_std_val)
     pf.create_uniform_particles(initial_state, initial_range)
+    return pf
 
-    input_vector = InputVector()
+
+def run_pf(states):
+    initial_range = [1.0, 1.0, 1.0]
+    meas_std_val = 0.005
+    num_particles = 250
     u_std = [0.005, 0.005, 0.005]
 
-    z = None  # measurement state vector
-    prev_z_update_t = 0.0
+    labels = [
+        "BACKGROUND",
+        "cozmo_cube",
+        "blue_cut_sphere",
+        "red_cut_sphere",
+        "blue_low_bin",
+        "red_low_bin",
+        "blue_cube",
+        "red_cube",
+    ]
+
+    pfs = {}
+
+    input_vector = InputVector()
 
     sim_start_t = states[0].stamp
     real_start_t = time.time()
 
-    plotter.init()
-    # input()
+    # plotter = ParticleFilterPlotter3D(10.0, 10.0, 3.0)
+    plotter = ParticleFilterPlotter2D(3.0, 3.0)
 
     for state in states:
         sim_time = state.stamp
@@ -96,23 +101,28 @@ def run_pf(states):
 
         if state.type == "odom":
             dt = input_vector.update(state)
-            pf.predict(input_vector.u, u_std, dt)
+            for pf in pfs.values():
+                pf.predict(input_vector.u, u_std, dt)
             plotter.update_odom(state)
-        # elif state.type == "blue_cut_sphere":
-        elif state.type == "red_cut_sphere":
+        elif state.type in labels:
+            name = state.type
             z = [state.x, state.y, state.z]
-            pf.update(z)
-            prev_z_update_t = sim_duration
-            plotter.update_measure(state)
+            if name not in pfs:
+                pf = init_pf(num_particles, meas_std_val, initial_range, z)
+                pfs[name] = pf
+            pfs[name].update(z)
+            plotter.update_measure(name, state)
 
-        pf.check_resample()
-
-        if sim_duration - prev_z_update_t > 0.5:
-            z = None  # hide measurement if one hasn't appeared in a while
+        for pf in pfs.values():
+            pf.check_resample()
 
         if sim_duration >= real_duration:  # if simulation time has caught up to real time, spend some time drawing
-            plotter.draw()
+            plotter.clear()
+            for name, pf in pfs.items():
+                plotter.draw(pf, name)
+            plotter.pause()
     plotter.stop()
+
 
 if __name__ == '__main__':
     main()
